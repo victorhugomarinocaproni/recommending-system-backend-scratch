05c68e51aa23b5f8878f9e4bb7b18b18c16c1834
Victor Caproni <victorcaproni04@gmail.com>
2025-04-21 14:56:50 -0300

    Changed the toy dataset script to generate the fake users json file in the '/data' directory. Also, added this directory to the gitignore file so that the json file won't be send to the repository.

--- src/toy_dataset.py
+++ src/toy_dataset.py
@@ -1,8 +1,8 @@
 import numpy as np
 import random
 import json
+import os
 
-# Ingredientes únicos do seu projeto
 ingredients_list = sorted([
     "aperol", "espumante", "água com gás", "laranja", "rum", "limão", "hortelã", "açúcar",
     "tequila", "licor de laranja", "leite de coco", "abacaxi", "vodka", "licor de pêssego",
@@ -10,7 +10,6 @@ ingredients_list = sorted([
     "cranberry", "campari", "cola", "suco de tomate", "molho inglês", "pimenta", "cachaça"
 ])
 
-# Quantidade de usuários a serem gerados
 NUM_USERS = 100
 
 def generate_users(num_users=NUM_USERS, seed=42):
@@ -19,12 +18,10 @@ def generate_users(num_users=NUM_USERS, seed=42):
 
     users = []
 
-    # Geração dos usuários
     for _ in range(num_users):
         # Gera um número de ingredientes preferidos para o usuário (entre 2 e 7)
         num_likes = int(np.clip(np.random.normal(loc=4, scale=1.5), 2, 7))
 
-        # Escolhe ingredientes aleatórios
         user_likes = random.sample(ingredients_list, num_likes)
         users.append(user_likes)
 
@@ -33,13 +30,13 @@ def generate_users(num_users=NUM_USERS, seed=42):
 if __name__ == "__main__":
     users = generate_users()
 
-    # Printar alguns exemplos
     for idx, user in enumerate(users[:5]):  # Mostrar só os 5 primeiros
         print(f"Usuário {idx+1}: {user}")
+        
+    os.makedirs("data", exist_ok=True) 
 
-    # Opcional: salvar em um arquivo JSON
-    with open("generated_users.json", "w", encoding="utf-8") as f:
-        json.dump(users, f, ensure_ascii=False, indent=2)
+    with open("data/generated_users.json", "w", encoding="utf-8") as f:
+        json.dump(users, f, ensure_ascii=False, indent=4)
 
     print(f"\n✅ Gerados {len(users)} perfis de usuários!")
-    print("Arquivo 'generated_users.json' salvo com sucesso.")
+    print("Arquivo 'generated_users.json' salvo no diretório '/data' com sucesso.")
